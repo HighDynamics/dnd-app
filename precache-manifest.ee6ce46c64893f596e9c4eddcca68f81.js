d8221be1566a99869e8d0c22e1ed91a95b177853
Daniel Denton <highdynamics@gmail.com>
2020-07-07 20:59:02 -0500

    Updates

---
+++ precache-manifest.ee6ce46c64893f596e9c4eddcca68f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078261a4297f8b5686461cad9215234f",
+    "url": "/dnd-app/index.html"
+  },
+  {
+    "revision": "3b4262dad8e2e5282dab",
+    "url": "/dnd-app/static/css/main.cba1fd48.chunk.css"
+  },
+  {
+    "revision": "bd30732b149e07df14dd",
+    "url": "/dnd-app/static/js/2.14070aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-app/static/js/2.14070aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4262dad8e2e5282dab",
+    "url": "/dnd-app/static/js/main.ed03a465.chunk.js"
+  },
+  {
+    "revision": "bf608ad41f6bb6f6156f",
+    "url": "/dnd-app/static/js/runtime-main.4de185bc.js"
+  }
+]);
