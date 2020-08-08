8c16c40109bf3f822ec6e6260ef6cf9b3cafef3c
Daniel Denton <highdynamics@gmail.com>
2020-08-07 19:10:37 -0500

    remove unused state variables, fix PrimaryModifier

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,59 +1,56 @@
-import React, { useState, useContext } from 'react';
-import { Character, ReadTossDice, rollDice } from './dnd.js'
+import React, { useContext } from "react";
+import { Character, ReadTossDice, rollDice } from "./dnd.js";
 
-const SkillsListItem = (props) => {
-  const character = useContext(Character)
+const SkillsListItem = props => {
+  const character = useContext(Character);
   const [rollResult, setRollResult] = useContext(ReadTossDice);
   // store props to make code simpler
-  const skills = props.skills
+  const skills = props.skills;
   // replace underscore with space and store
-  let formattedSkill = skills[0].replace(/_/g, ' ');
+  let formattedSkill = skills[0].replace(/_/g, " ");
   // update variable replacing (Know)ledge with :
-  formattedSkill = formattedSkill.replace(/ledge/g, ':')
+  formattedSkill = formattedSkill.replace(/ledge/g, ":");
   // store skill points separately
   const skillPoints = skills[1];
   // confirm class skill to add css class
-  function findClassSkills(skill){
-    if(character.classSkills.hasOwnProperty(skill)){
-      return 'classSkills'
-    }else{
-      return ''
+  function findClassSkills(skill) {
+    if (character.classSkills.hasOwnProperty(skill)) {
+      return "classSkills";
+    } else {
+      return "";
     }
   }
   return (
     <button
       className={`skills ${findClassSkills(skills[0])} ${skills[0]}`}
-      onClick={() => setRollResult(rollDice(20, skillPoints, formattedSkill))}>
-      {formattedSkill} | <span className='skillPoints'>{skillPoints}</span>
+      onClick={() => setRollResult(rollDice(20, skillPoints, formattedSkill))}
+    >
+      {formattedSkill} | <span className="skillPoints">{skillPoints}</span>
     </button>
-  )
-}
-const Skills = (props) =>  {
-  const character = useContext(Character)
+  );
+};
+const Skills = props => {
+  const character = useContext(Character);
   //put character's skills into array as [key, value]
-  let skillsArray = Object.keys(character.skills).map((skill) => {
-    return [skill, character.skills[skill]]
+  let skillsArray = Object.keys(character.skills).map(skill => {
+    return [skill, character.skills[skill]];
   });
   // put character's class skills into array as [key, value]
-  let classSkillsArray = Object.keys(character.classSkills).map((skill) => {
+  let classSkillsArray = Object.keys(character.classSkills).map(skill => {
     return [skill, character.classSkills[skill]];
   });
   // combine both arrays into new array, sorted alphabetically
   let allSkills = [...skillsArray, ...classSkillsArray].sort();
   // pass skills to child component
-  const skillsBlock = allSkills.map((s) =>
-    <SkillsListItem key={s} skills={s} />
+  const skillsBlock = allSkills.map(s => <SkillsListItem key={s} skills={s} />);
+  return (
+    <>
+      <h1 id="skillsHeader">Skills</h1>
+      <ul id="skillsListWrapper" style={{ listStyleType: "none" }}>
+        <div id="skillsWrapper">{skillsBlock}</div>
+      </ul>
+    </>
   );
-return(
-  <>
-    <h1 id='skillsHeader'>Skills</h1>
-    <ul id='skillsListWrapper' style={{listStyleType: 'none'}}>
-      <div id='skillsWrapper'>
-        {skillsBlock}
-      </div>
-    </ul>
-  </>
-  );
-}
+};
 
-export default Skills
+export default Skills;
--- src/components/SpellInfo.js
+++ src/components/SpellInfo.js
@@ -1,37 +1,47 @@
-import React, { useState, useContext } from 'react';
-import { Character, ToggleInfo, Selection } from './dnd.js'
+import React, { useContext } from "react";
+import { Character, ToggleInfo, Selection } from "./dnd.js";
 
-const SpellInfo = (props) => {
+const SpellInfo = props => {
   //bring in react context
-  const character = useContext(Character)
+  const character = useContext(Character);
   const [toggleInfo, setToggleInfo] = useContext(ToggleInfo);
-  const [selection, setSelection] = useContext(Selection);
+  const [selection] = useContext(Selection);
   //edit string for render
-  const formattedSpell = selection.replace(/_/g, ' ')
-  function getSpellLevel(selection){
+  const formattedSpell = selection.replace(/_/g, " ");
+  function getSpellLevel(selection) {
     let foundLevel = null;
-    Object.keys(character.magic.spells).forEach((level) => {
-      if(Object.values(character.magic.spells[level]).includes(selection)){
+    Object.keys(character.magic.spells).forEach(level => {
+      if (Object.values(character.magic.spells[level]).includes(selection)) {
         foundLevel = level;
       }
-    })
+    });
     return foundLevel;
   }
   return (
-    <div id='spellInfo' className='infoSheet'>
-      <button id='useSpell'>Use Spell</button>
-      <button id='closeButton' onClick={() => setToggleInfo('Off')}>x</button>
+    <div id="spellInfo" className="infoSheet">
+      <button id="useSpell">Use Spell</button>
+      <button id="closeButton" onClick={() => setToggleInfo("Off")}>
+        x
+      </button>
       <h3>{formattedSpell}</h3>
-      Level: {getSpellLevel(selection)}<br />
-      Components:<br />
-      Casting Time:<br />
-      Range:<br />
-      Target or Area:<br />
-      Duration:<br />
-      Saving Throw:<br />
-      SpellResistance:<br />
+      Level: {getSpellLevel(selection)}
+      <br />
+      Components:
+      <br />
+      Casting Time:
+      <br />
+      Range:
+      <br />
+      Target or Area:
+      <br />
+      Duration:
+      <br />
+      Saving Throw:
+      <br />
+      SpellResistance:
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default SpellInfo
+export default SpellInfo;
