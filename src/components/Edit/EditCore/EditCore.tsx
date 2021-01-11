a5d66ecc543629841c24f3d663e1111d774b1f6b
Daniel Denton <highdynamics@gmail.com>
2021-01-11 11:45:47 -0600

    reorder code

--- src/components/Edit/EditCore/EditCore.tsx
+++ src/components/Edit/EditCore/EditCore.tsx
@@ -21,6 +21,276 @@ import AbilityScoreForm from "./AbilityScoreForm";
 
 import "./EditCore.css";
 
+const ClassFormParent = ({ character }: { character: ICharacter }) => {
+  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
+    updatedCharacterState
+  );
+  const editedCharacter = clone(updatedCharacter);
+
+  const handleChange = (
+    setterFunction: (value: any) => void,
+    index: number
+  ): React.ChangeEventHandler => (e) => {
+    const value = e.target.value;
+    switch (e.target.name) {
+      case "classTitle":
+        setterFunction(value);
+        editedCharacter.class[index].name = value;
+        break;
+      case "level":
+        setterFunction(value);
+        editedCharacter.class[index].level = Number(value);
+        break;
+      default:
+        break;
+    }
+    setUpdatedCharacter(editedCharacter);
+  };
+
+  const handleNewClass: React.MouseEventHandler = (e) => {
+    editedCharacter.class.push({ name: "", level: 0 });
+    setUpdatedCharacter(editedCharacter);
+  };
+  const classes = Object.values(character.class).map((value) => {
+    return (
+      <ClassForm
+        key={value.name}
+        classItem={value}
+        handleEvent={handleChange}
+        character={character}
+      />
+    );
+  });
+
+  return (
+    <>
+      <fieldset className="formItemsContainer">
+        <legend className="legendHeader">Classes</legend>
+        {classes}
+        <br />
+        <button
+          onClick={handleNewClass}
+          className="defaultButton coreFormButton"
+        >
+          New Class
+        </button>
+      </fieldset>
+    </>
+  );
+};
+
+const TypesFormParent = ({ character }: { character: ICharacter }) => {
+  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
+    updatedCharacterState
+  );
+  const editedCharacter = clone(updatedCharacter);
+
+  const handleChange = (
+    setterFunction: (value: string) => void,
+    index: number
+  ): React.ChangeEventHandler => (e) => {
+    const value = e.target.value;
+    setterFunction(value);
+    editedCharacter.type[index] = value;
+    setUpdatedCharacter(editedCharacter);
+  };
+
+  const handleNewType: React.MouseEventHandler = (e) => {
+    editedCharacter.type.push("");
+    setUpdatedCharacter(editedCharacter);
+  };
+
+  const type = character.type.map((value) => (
+    <TypeForm
+      key={value}
+      type={value}
+      character={character}
+      handleEvent={handleChange}
+    />
+  ));
+
+  return (
+    <fieldset className="formItemsContainer">
+      <legend className="legendHeader">Types</legend>
+      {type}
+      <br />
+      <button className="defaultButton coreFormButton" onClick={handleNewType}>
+        New Type
+      </button>
+    </fieldset>
+  );
+};
+
+const HitPointsFormParent = ({ character }: { character: ICharacter }) => {
+  return (
+    <fieldset className="formItemsContainer">
+      <legend className="legendHeader">Hit Points</legend>
+      <HitPointsForm
+        hitPoints={character.hitPoints.total}
+        dieSize={character.hitPoints.dieSize}
+      />
+    </fieldset>
+  );
+};
+
+const AbilityScoreFormParent = ({ character }: { character: ICharacter }) => {
+  const primaryMod = character.abilities.primary;
+  const [primaryModifier, setPrimaryModifier] = useState(primaryMod);
+  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
+    updatedCharacterState
+  );
+  const editedCharacter = clone(updatedCharacter);
+
+  const abilityScore = Object.entries(character.abilities.score).map(
+    ([field, value]) => {
+      if (value === null) {
+        value = 0;
+      }
+      return (
+        <AbilityScoreForm
+          key={field}
+          field={field as keyof typeof character.abilities.score}
+          value={value}
+        />
+      );
+    }
+  );
+
+  function handleChange(e: React.ChangeEvent) {
+    setPrimaryModifier(e.target.value);
+    editedCharacter.abilities.primary = e.target.value;
+    setUpdatedCharacter(editedCharacter);
+  }
+  return (
+    <fieldset className="formItemsContainer">
+      <legend className="legendHeader">Ability Scores</legend>
+      <div className="abilityScoreFormContainer">{abilityScore}</div>
+      <br />
+      <div className="primaryModifier">
+        <label>
+          Primary Modifier:{" "}
+          <select
+            name="primaryModifier"
+            className="textInput"
+            value={primaryModifier}
+            onChange={handleChange}
+          >
+            <option value="strength">Strength</option>
+            <option value="dexterity">Dexterity</option>
+            <option value="constitution">Constitution</option>
+            <option value="intelligence">Intelligence</option>
+            <option value="wisdom">Wisdom</option>
+            <option value="charisma">Charisma</option>
+          </select>
+        </label>
+      </div>
+    </fieldset>
+  );
+};
+
+const ArmorClassFormParent = ({ character }: { character: ICharacter }) => {
+  const armorClass = Object.entries(
+    character.armorClass
+  ).map(([field, value]) => (
+    <ArmorClassForm
+      key={field}
+      field={field as keyof typeof character.armorClass}
+      value={value}
+    />
+  ));
+
+  return (
+    <fieldset className="formItemsContainer">
+      <legend className="legendHeader">Armor Class</legend>
+      <div className="armorClassFormContainer">{armorClass}</div>
+    </fieldset>
+  );
+};
+
+const SavesFormParent = ({ character }: { character: ICharacter }) => {
+  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
+    updatedCharacterState
+  );
+  const editedCharacter = clone(updatedCharacter);
+  const fieldPath = editedCharacter.defense.saves;
+  const handleChange = (
+    setterFunction:
+      | ((value: string | null) => void)
+      | ((value: number | null) => void),
+    fieldParent: keyof ICharacter["defense"]["saves"]
+  ): React.ChangeEventHandler => (e) => {
+    const value = e.target.value;
+    setterFunction(value);
+    switch (e.target.name) {
+      case "base":
+        fieldPath[fieldParent].base = Number(value);
+        break;
+      case "magic":
+        fieldPath[fieldParent].magic = Number(value);
+        break;
+      case "misc":
+        fieldPath[fieldParent].misc = Number(value);
+        break;
+      default:
+        break;
+    }
+    setUpdatedCharacter(editedCharacter);
+  };
+
+  const fortSave = Object.entries(character.defense.saves.fortitude).map(
+    ([field, value]) => (
+      <li key={field + "Fortitude"}>
+        <SavesForm
+          field={field}
+          value={value}
+          handleEvent={handleChange}
+          fieldParent="fortitude"
+        />
+      </li>
+    )
+  );
+  const reflexSave = Object.entries(character.defense.saves.reflex).map(
+    ([field, value]) => (
+      <li key={field + "Reflex"}>
+        <SavesForm
+          field={field}
+          value={value}
+          handleEvent={handleChange}
+          fieldParent="reflex"
+        />
+      </li>
+    )
+  );
+  const willSave = Object.entries(character.defense.saves.will).map(
+    ([field, value]) => (
+      <li key={field + "Will"}>
+        <SavesForm
+          field={field}
+          value={value}
+          handleEvent={handleChange}
+          fieldParent="will"
+        />
+      </li>
+    )
+  );
+  return (
+    <fieldset className="formItemsContainer">
+      <legend className="legendHeader">Saves</legend>
+      <div className="savesContainer">
+        <div className="savesItem">
+          Fortitude: <ul>{fortSave}</ul>
+        </div>
+        <div className="savesItem">
+          Reflex: <ul>{reflexSave}</ul>
+        </div>
+        <div className="savesItem">
+          Will: <ul>{willSave}</ul>
+        </div>
+      </div>
+    </fieldset>
+  );
+};
+
 const DefenseFormParent = () => {
   const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
     updatedCharacterState
@@ -95,8 +365,8 @@ const DefenseFormParent = () => {
 
   return (
     <>
-      <fieldset>
-        <legend>Defense</legend>
+      <fieldset className="formItemsContainer">
+        <legend className="legendHeader">Defense</legend>
         <div className="defenseContainer">
           <div className="defenseItem">
             <label>
@@ -111,271 +381,17 @@ const DefenseFormParent = () => {
             </label>
           </div>
           <div className="defenseItem">
-            Damage Reduction: <ul>{damageReduction}</ul>
+            <fieldset>
+              {" "}
+              <legend>Damage Reduction</legend> <ul>{damageReduction}</ul>
+            </fieldset>
           </div>
           <div className="defenseItem">
-            Energy Resistance: <ul>{energyResistance}</ul>
+            <fieldset>
+              <legend>Energy Resistance</legend> <ul>{energyResistance}</ul>
+            </fieldset>
           </div>
         </div>
-      </fieldset>
-    </>
-  );
-};
-
-const SavesFormParent = ({ character }: { character: ICharacter }) => {
-  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
-    updatedCharacterState
-  );
-  const editedCharacter = clone(updatedCharacter);
-  const fieldPath = editedCharacter.defense.saves;
-  const handleChange = (
-    setterFunction:
-      | ((value: string | null) => void)
-      | ((value: number | null) => void),
-    fieldParent: keyof ICharacter["defense"]["saves"]
-  ): React.ChangeEventHandler => (e) => {
-    const value = e.target.value;
-    setterFunction(value);
-    switch (e.target.name) {
-      case "base":
-        fieldPath[fieldParent].base = Number(value);
-        break;
-      case "magic":
-        fieldPath[fieldParent].magic = Number(value);
-        break;
-      case "misc":
-        fieldPath[fieldParent].misc = Number(value);
-        break;
-      default:
-        break;
-    }
-    setUpdatedCharacter(editedCharacter);
-  };
-
-  const fortSave = Object.entries(character.defense.saves.fortitude).map(
-    ([field, value]) => (
-      <li key={field + "Fortitude"}>
-        <SavesForm
-          field={field}
-          value={value}
-          handleEvent={handleChange}
-          fieldParent="fortitude"
-        />
-      </li>
-    )
-  );
-  const reflexSave = Object.entries(character.defense.saves.reflex).map(
-    ([field, value]) => (
-      <li key={field + "Reflex"}>
-        <SavesForm
-          field={field}
-          value={value}
-          handleEvent={handleChange}
-          fieldParent="reflex"
-        />
-      </li>
-    )
-  );
-  const willSave = Object.entries(character.defense.saves.will).map(
-    ([field, value]) => (
-      <li key={field + "Will"}>
-        <SavesForm
-          field={field}
-          value={value}
-          handleEvent={handleChange}
-          fieldParent="will"
-        />
-      </li>
-    )
-  );
-  return (
-    <fieldset>
-      <legend>Saves</legend>
-      <div className="savesContainer">
-        <div className="savesItem">
-          Fortitude: <ul>{fortSave}</ul>
-        </div>
-        <div className="savesItem">
-          Reflex: <ul>{reflexSave}</ul>
-        </div>
-        <div className="savesItem">
-          Will: <ul>{willSave}</ul>
-        </div>
-      </div>
-    </fieldset>
-  );
-};
-
-const ArmorClassFormParent = ({ character }: { character: ICharacter }) => {
-  const armorClass = Object.entries(
-    character.armorClass
-  ).map(([field, value]) => (
-    <ArmorClassForm
-      key={field}
-      field={field as keyof typeof character.armorClass}
-      value={value}
-    />
-  ));
-
-  return (
-    <fieldset>
-      <legend>Armor Class</legend>
-      <div className="armorClassFormContainer">{armorClass}</div>
-    </fieldset>
-  );
-};
-
-const HitPointsFormParent = ({ character }: { character: ICharacter }) => {
-  return (
-    <fieldset>
-      <legend>Hit Points</legend>
-      <HitPointsForm
-        hitPoints={character.hitPoints.total}
-        dieSize={character.hitPoints.dieSize}
-      />
-    </fieldset>
-  );
-};
-
-const AbilityScoreFormParent = ({ character }: { character: ICharacter }) => {
-  const primaryMod = character.abilities.primary;
-  const [primaryModifier, setPrimaryModifier] = useState(primaryMod);
-  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
-    updatedCharacterState
-  );
-  const editedCharacter = clone(updatedCharacter);
-
-  const abilityScore = Object.entries(character.abilities.score).map(
-    ([field, value]) => {
-      if (value === null) {
-        value = 0;
-      }
-      return (
-        <AbilityScoreForm
-          key={field}
-          field={field as keyof typeof character.abilities.score}
-          value={value}
-        />
-      );
-    }
-  );
-
-  function handleChange(e: React.ChangeEvent) {
-    setPrimaryModifier(e.target.value);
-    editedCharacter.abilities.primary = e.target.value;
-    setUpdatedCharacter(editedCharacter);
-  }
-  return (
-    <fieldset>
-      <legend>Ability Scores</legend>
-      <div className="abilityScoreFormContainer">{abilityScore}</div>
-      <br />
-      <div className="primaryModifier">
-        Primary Modifier:{" "}
-        <select
-          name="primaryModifier"
-          className="textInput"
-          value={primaryModifier}
-          onChange={handleChange}
-        >
-          <option value="strength">Strength</option>
-          <option value="dexterity">Dexterity</option>
-          <option value="constitution">Constitution</option>
-          <option value="intelligence">Intelligence</option>
-          <option value="wisdom">Wisdom</option>
-          <option value="charisma">Charisma</option>
-        </select>
-      </div>
-    </fieldset>
-  );
-};
-
-const TypesFormParent = ({ character }: { character: ICharacter }) => {
-  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
-    updatedCharacterState
-  );
-  const editedCharacter = clone(updatedCharacter);
-
-  const handleChange = (
-    setterFunction: (value: string) => void,
-    index: number
-  ): React.ChangeEventHandler => (e) => {
-    const value = e.target.value;
-    setterFunction(value);
-    editedCharacter.type[index] = value;
-    setUpdatedCharacter(editedCharacter);
-  };
-
-  const handleNewType: React.MouseEventHandler = (e) => {
-    editedCharacter.type.push("");
-    setUpdatedCharacter(editedCharacter);
-  };
-
-  const type = character.type.map((value) => (
-    <TypeForm
-      key={value}
-      type={value}
-      character={character}
-      handleEvent={handleChange}
-    />
-  ));
-
-  return (
-    <fieldset>
-      <legend>Types</legend>
-      {type}
-      <button onClick={handleNewType}>New Type</button>
-    </fieldset>
-  );
-};
-
-const ClassFormParent = ({ character }: { character: ICharacter }) => {
-  const [updatedCharacter, setUpdatedCharacter] = useRecoilState(
-    updatedCharacterState
-  );
-  const editedCharacter = clone(updatedCharacter);
-
-  const handleChange = (
-    setterFunction: (value: any) => void,
-    index: number
-  ): React.ChangeEventHandler => (e) => {
-    const value = e.target.value;
-    switch (e.target.name) {
-      case "classTitle":
-        setterFunction(value);
-        editedCharacter.class[index].name = value;
-        break;
-      case "level":
-        setterFunction(value);
-        editedCharacter.class[index].level = Number(value);
-        break;
-      default:
-        break;
-    }
-    setUpdatedCharacter(editedCharacter);
-  };
-
-  const handleNewClass: React.MouseEventHandler = (e) => {
-    editedCharacter.class.push({ name: "", level: 0 });
-    setUpdatedCharacter(editedCharacter);
-  };
-  const classes = Object.values(character.class).map((value) => {
-    return (
-      <ClassForm
-        key={value.name}
-        classItem={value}
-        handleEvent={handleChange}
-        character={character}
-      />
-    );
-  });
-
-  return (
-    <>
-      <fieldset>
-        <legend>Classes</legend>
-        {classes}
-        <button onClick={handleNewClass}>New Class</button>
       </fieldset>
     </>
   );
@@ -397,7 +413,7 @@ const EditCore = (props) => {
 
   return (
     <>
-      <button onClick={() => setMainContent("More")}>
+      <button onClick={() => setMainContent("More")} className="backButton">
         {" "}
         <i className="fas fa-arrow-left"></i>
       </button>
@@ -414,7 +430,7 @@ const EditCore = (props) => {
         <ArmorClassFormParent character={character} />
         <SavesFormParent character={character} />
         <DefenseFormParent />
-        <input type="submit" />
+        <input className="defaultButton coreFormButton" type="submit" />
       </form>
     </>
   );
