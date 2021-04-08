9ba38238120e0b64624fe48e2deb7f379bf82242
Daniel Denton <highdynamics@gmail.com>
2021-04-08 14:10:08 -0500

    type definition for ICharacter

--- src/global-storage.d.ts
+++ src/global-storage.d.ts
@@ -2,16 +2,120 @@ import characters from "./server/characters";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 declare global {
-  type ICharacter = typeof characters[number];
+  type ICharacter = {
+    name: string,
+    type: string[],
+    hitPoints: {
+      dieSize: number,
+      total: number,
+    },
+    armorClass: {
+      armor: number,
+      shield: number,
+      dexterity: number,
+      size: number,
+      naturalArmor: number,
+      deflection: number,
+      misc: number,
+    },
+    defense: {
+      damageReduction: {
+        amount: number,
+        weakness: string,
+      },
+      spellResistance: number,
+      energyResistance: {
+        acid: number|null,
+        cold: number|null,
+        electricity: number|null,
+        fire: number|null,
+        sonic: number|null,
+      },
+      saves: {
+        fortitude: Save,
+        reflex: Save,
+        will: Save,
+        }
+      },
+    size: string,
+    alignment: string,
+    abilities:{
+      score:{
+        strength: number|null,
+        dexterity: number|null,
+        constitution: number|null,
+        intelligence: number|null,
+        wisdom: number|null,
+        charisma: number|null,
+      },
+      primary: "strength"
+      | "dexterity"
+      | "constitution"
+      | "intelligence"
+      | "wisdom"
+      | "charisma",
+    },
+    speed: number,
+    class: DNDClass[],
+    skills: Skill[],
+    characterAbilities:{
+      active: SpecialAbilities[],
+      passive: SpecialAbilities[],
+    },
+    magic: {
+      spellcaster: boolean,
+      type: {
+        arcane: boolean,
+        divine: boolean,
+      }
+      sla_refs: ISLARef[],
+      spell_refs: ISpellRef[],
+      spellsPerDay: {
+        zero: number,
+        one: number,
+        two: number,
+        three: number,
+        four: number,
+        five: number,
+        six: number,
+        seven: number,
+        eight: number,
+        nine: number,
+      },
+    },
+    items: string[],
+  };
 
   namespace ICharacter {
-    type Skill = ICharacter["skills"][number];
+    type EnergyResistance = {[name: string]: number|null}
+    type Save = {
+      base: number,
+          magic: number,
+          misc: number,
+    }
+    type DNDClass = {name: string, level: number}
+    type Skill = {
+      name: string,
+      ability: string,
+      ranks: number,
+      miscModifier: number,
+      classSkill: boolean,
+      armorCheck: boolean,
+      display: boolean
+    }
+    type SpecialAbilities = {name: string, type: string}
   }
 
   type ISpellRef ={
     id: string;
     level: number;
     innate: boolean;
+  }
+  type ISLARef = {
+    id: string,
+    level: number,
+    uses: number,
+    frequency: string
   }
   
   type ISpell = {
