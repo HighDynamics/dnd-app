65d7919eac5ef15485addb6737dc6011bb479829
Daniel Denton <highdynamics@gmail.com>
2021-04-10 22:29:22 -0500

    css

--- src/components/AddCharacter/AddCharacter.tsx
+++ src/components/AddCharacter/AddCharacter.tsx
@@ -6,7 +6,7 @@ const AddCharacter = () => {
   const setMainContent = useSetRecoilState(mainContentState);
   return (
     <>
-      <button onClick={() => setMainContent("More")}>
+      <button className="backButton" onClick={() => setMainContent("More")}>
         {" "}
         <i className="fas fa-arrow-left"></i>
       </button>
