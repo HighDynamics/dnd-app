fb5d56d1f113b04c90cb77105fdaca8d0285f438
Daniel Denton <highdynamics@gmail.com>
2021-04-10 22:32:17 -0500

    add new character template

--- src/server/characters/index.ts
+++ src/server/characters/index.ts
@@ -1,6 +1,7 @@
 import Arn from "./Arn_Hachem";
 import Ig from "./Ig_Nallah";
+import New from "./New_Character";
 
-const characters = [Arn, Ig];
+const characters = [Arn, Ig, New];
 
 export default characters;
