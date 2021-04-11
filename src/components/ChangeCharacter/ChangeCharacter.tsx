20649e81fbeea5f014ce65f6316fdfd78b462e0d
Daniel Denton <highdynamics@gmail.com>
2021-04-10 22:30:32 -0500

    begin adding support for new character creation

--- src/components/ChangeCharacter/ChangeCharacter.tsx
+++ src/components/ChangeCharacter/ChangeCharacter.tsx
@@ -1,13 +1,21 @@
 import { useRecoilState, useSetRecoilState } from "recoil";
-import { characterState, updatedCharacterState } from "../../recoilState";
+import {
+  characterState,
+  updatedCharacterState,
+  mainContentState,
+} from "../../recoilState";
 import useSWR from "swr";
 
 const ChangeCharacter = () => {
   const [character, setCharacter] = useRecoilState(characterState);
   const setUpdatedCharacter = useSetRecoilState(updatedCharacterState);
+  const setMainContent = useSetRecoilState(mainContentState);
   const { data: charactersResponse } = useSWR<IServer.GetCharacters.Response>(
     "/api/characters"
   );
+  function handleNewCharacter() {
+    setMainContent("EditCore");
+  }
   function setCharacterById(id: string): ICharacter {
     let selectedCharacter: ICharacter = character;
     for (const char of charactersResponse?.characters) {
@@ -17,6 +25,8 @@ const ChangeCharacter = () => {
     }
     setCharacter(selectedCharacter);
     setUpdatedCharacter(selectedCharacter);
+    id === "0" ? handleNewCharacter() : setMainContent("Skills");
+    setMainContent("Skills");
   }
   const availableCharacters = charactersResponse?.characters.map((char) => (
     <button
