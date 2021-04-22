6a3074e1c232719ae2642607058272531910cc17
Daniel Denton <highdynamics@gmail.com>
2021-04-22 12:26:15 -0500

    add item info

--- src/server/items/index.ts
+++ src/server/items/index.ts
@@ -2,6 +2,9 @@ const itemCompendium = [
   {
     id: "1",
     name: "Nolzur's Orb",
+    weight: 1,
+    casterLevel: 15,
+    price: "60,400 gp",
     description:
       "This crystal orb, awash with a swirl of colors at its center, bears a crack along one side. The colors within seem to leak out, like paint. Nolzur of Radigast City became a famed illusionist and alchemist as a member of the Company of Seven, but his legacy is often overshadowed by those of his compatriots. Nolzur and Keoghtom fell out more than a century ago, as the latter reputedly borrowed one of Nolzur's formulas to aid in his creation of the celebrated ointment that bears his name. Nolzur has yet to forgive him and his reclusion has caused his fame to fade over time. Few know that he began life as a thief or that his early travels took him far and wide, including to Blackmoor and the Pirate Isles of the Densac Gulf. He is most famous for the magical pigments that he developed, but he has numerous creations to his name, including this silvery sphere.\nNolzur's orb continually seeps oily paints that act as Nolzur's marvelous pigments 1/day. The paints issuing from the orb may also be used in creating elaborate guises, granting you a +10 competence bonus on all Disguise checks for up to 30 minutes, so long as the paints remain dry. Finally, you can fling the paints 1/day to create a prismatic spray with a range of 60 feet. If the paints are stored in any other device or container they lose all properties except those normally bestowed by Nolzur's marvelous pigments.\nstrong; (DC 22) conjuration and illusion CL 15th; Craft Wondrous Item, disguise self, major creation, prismatic spray. 30,200 gp, 2,416 XP. Weight: 1 lb. Price: 60,400 gp.\nSource: Dragon #359",
   },
