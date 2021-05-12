a932f6268ccf7870e70645cca9cde1bfcd85251f
Daniel Denton <highdynamics@gmail.com>
2021-05-12 11:23:14 -0500

    add new character, adjust fields in New_Character

--- src/server/characters/index.ts
+++ src/server/characters/index.ts
@@ -1,7 +1,8 @@
 import Arn from "./Arn_Hachem";
 import Ig from "./Ig_Nallah";
+import Zachariah from "./Zachariah_Asalam";
 import New from "./New_Character";
 
-const characters = [Arn, Ig, New];
+const characters = [Arn, Ig, Zachariah, New];
 
 export default characters;
