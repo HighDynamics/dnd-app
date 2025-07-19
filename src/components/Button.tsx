dcb62f903d558f68dc4a3b6b234c4ff7055b6707
Daniel Denton <highdynamics@gmail.com>
2025-07-19 18:54:26 -0500

    Pass disabled param

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import { combine as c } from "../lib";
 export function Button(p: React.HTMLProps<HTMLButtonElement>) {
   return (
     <button
+      disabled={p.disabled}
       onClick={p.onClick}
       className={c(
         "rounded-sm bg-fuchsia-950 px-2 py-1 outline outline-1 outline-stone-100",
