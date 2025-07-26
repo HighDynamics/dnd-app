4e92348065c26d34e1918074503bb2d62a37c171
Daniel Denton <highdynamics@gmail.com>
2025-07-26 12:34:10 -0500

    Redraw Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export function Button(p: React.HTMLProps<HTMLButtonElement>) {
       disabled={p.disabled}
       onClick={p.onClick}
       className={c(
-        "rounded-sm bg-fuchsia-950 px-2 py-1 outline outline-1 outline-stone-100",
+        "rounded-sm bg-fuchsia-950 bg-opacity-50 px-2 py-1 outline outline-1 outline-stone-800 transition-all duration-100 active:bg-fuchsia-800 active:outline-stone-300",
         p.className,
       )}
     >
