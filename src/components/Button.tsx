f108f2f42d62cc9657a60485e9c0658f49e3dcdf
Daniel Denton <highdynamics@gmail.com>
2025-07-27 16:54:44 -0500

    Add Button disabled styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,7 @@ export function Button(p: React.HTMLProps<HTMLButtonElement>) {
       onClick={p.onClick}
       className={c(
         "rounded-sm bg-fuchsia-950 bg-opacity-50 px-2 py-1 outline outline-1 outline-stone-800 transition-all duration-100 active:bg-fuchsia-800 active:outline-stone-300",
+        p.disabled && "cursor-not-allowed opacity-30 !outline-none",
         p.className,
       )}
     >
