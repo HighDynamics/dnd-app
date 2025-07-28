63e03cbf9369d7c4e8c235e1fe979cf9d0dae690
Daniel Denton <highdynamics@gmail.com>
2025-07-27 19:42:12 -0500

    Add Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import { combine as c } from "../lib";
+
+export function Input(p: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      {...p}
+      data-1p-ignore
+      data-lpignore
+      data-protonpass-ignore
+      autoComplete="off"
+      className={c(
+        "rounded-sm bg-black bg-opacity-50 px-2 py-1 outline outline-1 outline-stone-800 transition-all duration-100 focus:bg-fuchsia-950 focus:outline-stone-300",
+        p.className,
+      )}
+    />
+  );
+}
