ad4c0c0781d3a868cccd221b92c57f1a4de790ce
Daniel Denton <highdynamics@gmail.com>
2025-11-06 16:47:13 -0600

    Updates

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'dnd-app-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        // Optionally cache new requests here if desired
+        return response;
+      });
+    })
+  );
+});
