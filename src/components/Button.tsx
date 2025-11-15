ae3c85baf742f1364d4582c67b71cad9d30f6837
Daniel Denton <highdynamics@gmail.com>
2025-11-15 16:40:36 -0600

    Add button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export function Button(p: React.HTMLProps<HTMLButtonElement>) {
       disabled={p.disabled}
       onClick={p.onClick}
       className={c(
-        "rounded-xs bg-fuchsia-950 bg-opacity-50 px-2 py-1 outline-solid outline-1 outline-stone-800 transition-all duration-100 active:bg-fuchsia-800 active:outline-stone-300",
+        "cursor-pointer rounded-xs bg-fuchsia-950 bg-opacity-50 px-2 py-1 outline-solid outline-1 outline-stone-800 transition-all duration-100 active:bg-fuchsia-800 active:outline-stone-300",
         p.disabled && "cursor-not-allowed opacity-30 outline-hidden!",
         p.className,
       )}
